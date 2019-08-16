14bc194282e061ccad7ba69bb21fc7acce786c8f
GYAN RANJAN <42690317+gyan3098@users.noreply.github.com>
2019-08-16 12:19:56 +0530

    Add files via upload

---
+++ Chat/chat1.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+
+# sm = ScreenManager()
+
+class ChatApp(App):
+    pass
+
+# kv = Builder.load_file("chat.kv")
+
+if __name__=="__main__":
+    ChatApp().run()
